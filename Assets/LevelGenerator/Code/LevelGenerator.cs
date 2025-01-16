126912597ed03fc634f27db7d0c45bdf22f843db
Cats <tomi.sepanmaa@gmail.com>
2025-01-16 17:17:44 +0200

    Added visualisation for room layout for debug purposes

--- Assets/LevelGenerator/Code/LevelGenerator.cs
+++ Assets/LevelGenerator/Code/LevelGenerator.cs
@@ -1,7 +1,5 @@
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Rendering.Universal;
-using UnityEngine.UIElements;
 using static LevelGenerator.Enums;
 
 namespace LevelGenerator
@@ -16,17 +14,18 @@ namespace LevelGenerator
         private static bool m_isLevelGenerated;
         //private static Rooms m_roomPrefabs;
         //private static Dictionary m_generatedRooms;
-        private static int m_roomCount;
+        private static int m_currentFloorRoomCount;
         public static int[,] m_floorLayout;
         private static int m_maxXSize = 11;
-        private static int m_maxYSize = 9;
+        private static int m_maxYSize = 11;
         private static int m_currentRoomIndex;
+        private static List<(int x, int y)> m_roomLocations = new List<(int x, int y)>();
 
         public static void GenerateLevel(Difficulty _difficulty, int _currentFloor)
         {
             IsLevelAlreadyGenerated();
-            m_roomCount = CalculateRoomCount(_difficulty, _currentFloor);
-            Debug.Log("m_currentFloorSize: " + m_roomCount);
+            m_currentFloorRoomCount = CalculateRoomCount(_difficulty, _currentFloor);
+            Debug.Log("m_currentFloorRoomCount: " + m_currentFloorRoomCount);
             m_floorLayout = new int[m_maxXSize, m_maxYSize];
             RandomWalkFromEntranceToBoss();
         }
@@ -51,15 +50,22 @@ namespace LevelGenerator
 
         private static void RemoveLevel()
         {
-            m_roomCount = 0;
+            m_currentFloorRoomCount = 0;
             m_isLevelGenerated = false;
             m_currentRoomIndex = 0;
+            m_roomLocations.Clear();
+
+            // Debug
+            for (int i = 0; i < cubes.transform.childCount; i++)
+            {
+                Object.Destroy(cubes.transform.GetChild(i).gameObject);
+            }
         }
 
         public static void RandomWalkFromEntranceToBoss()
         //public static void RandomWalkFromEntranceToBoss()
         {
-            if (m_currentRoomIndex < m_roomCount)
+            if (m_currentRoomIndex < m_currentFloorRoomCount)
             {
                 int debugExitLoop = 0;
                 do
@@ -74,25 +80,42 @@ namespace LevelGenerator
                         Debug.LogError("Breaking the debugExitLoop loop. Too many tries.");
                         break;
                     }
-                } while (m_currentRoomIndex < m_roomCount);
+                } while (m_currentRoomIndex < m_currentFloorRoomCount);
 
-                // Debug
-                string printableString = "";
-                for (int y = 0; y < m_floorLayout.GetLength(1); y++)
-                {
-                    for (int x = 0; x < m_floorLayout.GetLength(0); x++)
-                    {
-                        printableString += m_floorLayout[x, y].ToString() + " ";
-                    }
-                    printableString += "\n";
-                }
-                Debug.Log(printableString);
+                DebugVisualiseLayout();
+
                 m_isLevelGenerated = true;
             }
             else
             {
                 Debug.LogWarning("Can't generate more. The floor layout is completed.");
             }
+        }
+
+        private static GameObject cubes = new GameObject("Cubes");
+        private static void DebugVisualiseLayout()
+        {
+            string printableString = "";
+            for (int y = 0; y < m_floorLayout.GetLength(1); y++)
+            {
+                for (int x = 0; x < m_floorLayout.GetLength(0); x++)
+                {
+                    printableString += m_floorLayout[x, y].ToString() + " ";
+                }
+                printableString += "\n";
+            }
+            Debug.Log(printableString);
+
+            foreach (var room in m_roomLocations)
+            {
+                GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                cube.transform.localScale = new Vector3(0.9f, 0.9f, 0.1f);
+                cube.transform.localPosition = new Vector3(room.x, room.y);
+                cube.transform.SetParent(cubes.transform);
+            }
+
+
+            // TODO: Place 1x1 cubes to the world for visual debug purposes
         }
 
         private static int[] m_lastPosition = new int[2];
@@ -171,8 +194,8 @@ namespace LevelGenerator
             m_lastPosition[0] = xPosition;
             m_lastPosition[1] = yPosition;
             Debug.Log($"LastPosition: {m_lastPosition[0] + 1}, {m_lastPosition[1] + 1}");
-            //_currentRoomIndex++;
             m_currentRoomIndex++;
+            m_roomLocations.Add((xPosition, yPosition));
         }
 
         private static bool IsValid(int[,] _directions, int _direction, int _xPosition, int _yPosition)
